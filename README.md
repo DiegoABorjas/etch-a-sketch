0e010598a244d39aacf33cf2fdb2ffa47d89ce71
Diego Borjas <diegoborjas@gmail.com>
2022-01-27 17:01:59 -0600

    completed initial functionality of the project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # etch-a-sketch
 Browser version of something between a sketchpad and an Etch-A-Sketch
+Using HTML CSS & JavaScript
+
+Users should be able to "draw" by hovering over the board.
+Also there will be a reset button to clear the board completely.
+Finally there will be a slider that will determine the size of the board. 
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const grid = document.querySelector('.container');
 const resetButton = document.querySelector(".reset");
-const range = document.querySelector('.range')
+const range = document.querySelector('.range');
+const square = document.querySelector("div");
+const rangeInput = document.getElementById("range")
 
+//Function creates the initial Grid
 function createGrid() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
@@ -10,25 +13,42 @@ function createGrid() {
     }
 }
 
+//Function updates the Grid size
 function updateGrid(n) {
-    grid.innerHTML = "";
-    for (let i = 0; i < n; i++) {
+    grid.innerHTML = '';
+    for (let i = 0; i < n*n; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         grid.appendChild(div);
     }
 }
 
-const square = document.querySelector("div")
+//Code to paint the squares as the user mouseover them.
 square.addEventListener('mouseover', function (event) {
-    event.target.classList.replace('square', 'color')
+    event.target.classList.replace('square', 'color');
 })
 
+//Reset button clears the Grid and restores its original value.
 resetButton.addEventListener('click', function (event) {
-    grid.innerHTML = "";
-    createGrid();
+    grid.innerHTML = '';
+    for (let i = 0; i < rangeInput.value * rangeInput.value; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        grid.appendChild(div);
+        grid.setAttribute('style', `grid-template-columns: repeat(${rangeInput.value}, 2fr); grid-template-rows: repeat(${rangeInput.value}, 2fr);`);
+        
+    }
 })
 
-
+rangeInput.addEventListener('input', function () {
+    grid.innerHTML = ''
+    for (let i = 0; i < rangeInput.value * rangeInput.value; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        grid.appendChild(div);
+        grid.setAttribute('style', `grid-template-columns: repeat(${rangeInput.value}, 2fr); grid-template-rows: repeat(${rangeInput.value}, 2fr);`);
+        
+    }
+});
 
 createGrid();
